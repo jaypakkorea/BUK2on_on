abdc3cd758e675ab0dc3bf5ecb52c34d6b4ddfe4
jaypakkorea <pakjeoungho@gmail.com>
2022-10-22 23:28:36 +0900

    2nd

--- BUK2on_on/static/js/main.js
+++ BUK2on_on/static/js/main.js
@@ -1,12 +1,16 @@
-let mainText = document.querySelector("h1");
+let whoweare = document.querySelector("h3");
+
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY
     console.log("scrollY", value);
 
-    if(value>50){
-        mainText.style.animation='disappear 1s ease-out forwards';
+
+    if(value>300){
+        whoweare.style.animation='disappear 1s ease-out forwards';
     }else{
-        mainText.style.animation='slide 1s ease-out'
+        whoweare.style.animation='slide 1s ease-out'
     }
 });
+
+
