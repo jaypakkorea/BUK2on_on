8554d0082fdd657931f1b2f4d5d8eb182e17faf4
eunjin <yej6642@naver.com>
2022-11-01 14:06:18 +0900

    hi

--- BUK2on_on/static/js/main.js
+++ BUK2on_on/static/js/main.js
@@ -14,3 +14,27 @@ window.addEventListener('scroll', function() {
 
 
 
+const modal = document.querySelector("#modal_detail_feed");
+const buttonAddFeed = document.querySelector("#detail_feed");
+buttonAddFeed.addEventListener("click", e => {
+    modal.style.top = window.pageYOffset + 'px';
+    modal.style.display = "flex";
+    document.body.style.overflowY = "hidden"; 
+});
+
+
+// const buttonCloseModal = document.querySelector("#close_modal");
+// document.addEventListener("click", e => {
+//     if(buttonAddFeed == true) {
+//         if(e.target.id != 'detail_feed') {
+//             modal.style.display = "none";
+//             document.body.style.overflowY = "visible";
+//         }
+//     }
+// });
+
+const buttonCloseModal = document.querySelector("#close_modal");
+buttonCloseModal.addEventListener("click", e => {
+    modal.style.display = "none";
+    document.body.style.overflowY = "visible";
+});
